d77d00854b22bd6f3e0695d9d283c7ef71af20b3
Andrsss122 <104798449+Andrsss122@users.noreply.github.com>
2022-07-27 10:53:12 -0600

    añadiendo documento

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Buscador from './componente/Buscador';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Buscador></Buscador>
+
     </div>
   );
 }
---
+++ src/componente/Buscador.jsx
@@ -0,0 +1,10 @@
+const Buscador = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>Hola</p>
+        </div>
+    )
+}
+
+export default Buscador;
